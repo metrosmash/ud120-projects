cd25cc84924cc7326cacbc30d91ab327b72fbb9e
Ajiboye Toluwalase <74120209+metrosmash@users.noreply.github.com>
2022-09-15 04:53:16 +0100

    Tried the naive bayes author id problem

--- naive_bayes/nb_author_id.py
+++ naive_bayes/nb_author_id.py
@@ -24,9 +24,22 @@ features_train, features_test, labels_train, labels_test = preprocess()
 
 ##############################################################
 # Enter Your Code Here
+from sklearn.naive_bayes import GaussianNB
+gnb = GaussianNB()
 
 
+t0 = time()
+     gnb.fit(features_train,labels_train)
+ print("Training Time:", round(time()-t0, 3), "s")
 
+y_pred_gnb = gnb.predict(features_test)
+ t0 = time()
+    y_pred_gnb = gnb.predict(features_test)
+ print("Predicting Time:", round(time()-t0, 3), "s")
+
+from sklearn.metrics import accuracy_score
+ac_sc = accuracy_score(labels_test,y_pred_gnb)
+print('The Accuracy of This model is :',y_pred_gnb)
 ##############################################################
 
 ##############################################################
