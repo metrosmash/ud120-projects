e3fa2b4231be6982a922fd5225658bca31779dc2
Ajiboye Toluwalase <74120209+metrosmash@users.noreply.github.com>
2022-09-16 05:00:23 +0100

    solved the naive bayes mini project

--- naive_bayes/nb_author_id.py
+++ naive_bayes/nb_author_id.py
@@ -29,13 +29,13 @@ gnb = GaussianNB()
 
 
 t0 = time()
-     gnb.fit(features_train,labels_train)
- print("Training Time:", round(time()-t0, 3), "s")
+gnb.fit(features_train,labels_train)
+print("Training Time:", round(time()-t0, 3), "s")
 
 y_pred_gnb = gnb.predict(features_test)
- t0 = time()
-    y_pred_gnb = gnb.predict(features_test)
- print("Predicting Time:", round(time()-t0, 3), "s")
+t0 = time()
+y_pred_gnb = gnb.predict(features_test)
+print("Predicting Time:", round(time()-t0, 3), "s")
 
 from sklearn.metrics import accuracy_score
 ac_sc = accuracy_score(labels_test,y_pred_gnb)
