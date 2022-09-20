3ba8e82acf7f76ee6226d339b97f67e0b2865813
Ajiboye Toluwalase <74120209+metrosmash@users.noreply.github.com>
2022-09-20 01:13:16 +0100

    Update svm_author_id.py
    i solved the svm mini project

--- svm/svm_author_id.py
+++ svm/svm_author_id.py
@@ -22,6 +22,25 @@ features_train, features_test, labels_train, labels_test = preprocess()
 
 #########################################################
 ### your code goes here ###
+features_train = features_train[:int(len(features_train)/100)]
+labels_train = labels_train[:int(len(labels_train)/100)]
+
+from sklearn.svm import SVC
+svm = SVC(kernel = 'sigmoid',C = 1,gamma = 'scale')
+
+
+t0 = time()
+svm.fit(features_train,labels_train)
+print("Training Time:", round(time()-t0, 3), "s")
+
+t0 = time()
+y_pred_svm = svm.predict(features_test)
+print("Predicting Time:", round(time()-t0, 3), "s")
+
+from sklearn.metrics import accuracy_score
+acc = accuracy_score(labels_test,y_pred_svm)
+print('The accuracy of the svm model is :',acc)
+#sigmoid_kernel = 0.90 ,rbf_kernel = 0.88,poly_kernel = 0.61,linear_kernel = 0.884
 
 
 #########################################################
