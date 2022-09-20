ff52efc0529794d1fa01b49d56a6755bf7651114
Ajiboye Toluwalase <74120209+metrosmash@users.noreply.github.com>
2022-09-20 01:16:39 +0100

    Update dt_author_id.py
    i solved the Decision tree mini project

--- decision_tree/dt_author_id.py
+++ decision_tree/dt_author_id.py
@@ -24,6 +24,22 @@ features_train, features_test, labels_train, labels_test = preprocess()
 
 #########################################################
 ### your code goes here ###
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.metrics import accuracy_score
+
+dtc = DecisionTreeClassifier(max_depth = 150,min_samples_split= 300,criterion='entropy')
+
+t1 = time()
+dtc.fit(features_train,labels_train)
+print("Training Time:", round(time()-t1, 3), "s")
+
+t1 =  time()
+y_pred_dtc = dtc.predict(features_test)
+print('Predicting time:',round(time()-t1, 3), 's')
+
+acc_dtc = accuracy_score(labels_test,y_pred_dtc)
+print('the accuracy of the decision tree is :', acc_dtc)
+
 
 
 #########################################################
